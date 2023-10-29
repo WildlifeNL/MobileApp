7eeb3cc68c8dbde4b0c8138e1957225fdb363615
Dylan Nas <432592@student.fontys.nl>
2023-10-29 21:53:06 +0100

    fix: updated models

---
+++ lib/models/data/sighting.dart
@@ -0,0 +1,33 @@
+import 'package:uuid/uuid.dart';
+import 'package:wildlife_nl_app/models/data/geojson/point.dart';
+import 'package:wildlife_nl_app/models/data/interaction.dart';
+
+var uuid = const Uuid();
+
+class Sighting {
+  final String id = uuid.v4();
+  final String animal;
+  final int animalCount;
+  final String locationName;
+  final DateTime dateTime;
+  final Point location;
+
+  Sighting({
+    required this.animal,
+    required this.animalCount,
+    required this.locationName,
+    required this.dateTime,
+    required this.location,
+  });
+
+  factory Sighting.fromInteraction(Interaction interaction) => Sighting(
+      animal: "Deer",
+      animalCount: (interaction.animalCount ?? 0) +
+          (interaction.animalJuvenileCount ?? 0),
+      locationName: "Not available",
+      dateTime: interaction.interactionTime,
+      location: Point(coordinates: [
+        interaction.interactionLat ?? 0,
+        interaction.interactionLon ?? 0
+      ]));
+}
