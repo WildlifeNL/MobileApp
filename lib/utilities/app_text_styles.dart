b59ec2d405939e0b2b5f90cd93685de494f3aeba
Dylan Nas <432592@student.fontys.nl>
2023-11-09 14:31:19 +0100

    chore: added styling to text styles

--- lib/utilities/app_text_styles.dart
+++ lib/utilities/app_text_styles.dart
@@ -21,17 +21,62 @@ class AppTextStyle {
     var font = const TextStyle(fontFamily: "Verdana");
 
     return AppTextStyle(
-      headerLarge: font.copyWith(),
-      headerMedium: font.copyWith(),
-      headerSmall: font.copyWith(),
-      cardTitle: font.copyWith(),
-      paragraph: bodyFont(),
-      paragraphMedium: bodyFont(),
-      bold: bodyFont(),
-      hyperlink: bodyFont(),
-      buttonText: bodyFont(),
+      headerLarge: font.copyWith(
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+        height: 1.224,
+        letterSpacing: 0,
+      ),
+      headerMedium: font.copyWith(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 0,
+      ),
+      headerSmall: font.copyWith(
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 0,
+      ),
+      cardTitle: font.copyWith(
+        fontSize: 14,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 0,
+      ),
+      paragraph: bodyFont(
+        fontSize: 12,
+        letterSpacing: 0,
+      ),
+      paragraphMedium: bodyFont(
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0,
+      ),
+      bold: bodyFont(
+        fontSize: 12,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 0,
+      ),
+      hyperlink: bodyFont(
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0,
+      ),
+      buttonText: bodyFont(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0,
+      ),
     );
   }
 
-  AppTextStyle({required this.headerLarge, required this.headerMedium, required this.headerSmall, required this.cardTitle, required this.paragraph, required this.paragraphMedium, required this.bold, required this.hyperlink, required this.buttonText});
+  AppTextStyle(
+      {required this.headerLarge,
+      required this.headerMedium,
+      required this.headerSmall,
+      required this.cardTitle,
+      required this.paragraph,
+      required this.paragraphMedium,
+      required this.bold,
+      required this.hyperlink,
+      required this.buttonText});
 }
