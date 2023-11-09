50b71acbed36c972ca9e5527cb47ea3afbdd3fbd
Kevin v Loon <74180193+KvnvL@users.noreply.github.com>
2023-11-09 14:41:08 +0100

    chore: text styles usage fixed

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wildlife_nl_app/generated/l10n.dart';
 import 'package:wildlife_nl_app/nav.dart';
+import 'package:wildlife_nl_app/utilities/app_styles.dart';
 
 import 'utilities/app_colors.dart';
 
@@ -12,44 +13,46 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp(
-        localizationsDelegates: const [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          Locale('en'),
-          Locale('nl'),
-          Locale('de'),
-        ],
-        debugShowCheckedModeBanner: false,
-        title: "WildlifeNL",
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-          navigationBarTheme: NavigationBarThemeData(
-            iconTheme: MaterialStateProperty.resolveWith((state) {
-              if (state.contains(MaterialState.selected)) {
-                return const IconThemeData(color: AppColors.primary);
-              }
-              return IconThemeData(color: Colors.black.withOpacity(0.7));
-            }),
-            labelTextStyle: MaterialStateProperty.resolveWith((state) {
-              if (state.contains(MaterialState.selected)) {
-                return const TextStyle(
-                    color: AppColors.primary, fontSize: 12, height: 0.7);
-              }
-              return TextStyle(
-                  color: Colors.black.withOpacity(0.7),
-                  fontSize: 12,
-                  height: 0.7);
-            }),
+    return AppStyleProvider(
+      child: ProviderScope(
+        child: MaterialApp(
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en'),
+            Locale('nl'),
+            Locale('de'),
+          ],
+          debugShowCheckedModeBanner: false,
+          title: "WildlifeNL",
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+            navigationBarTheme: NavigationBarThemeData(
+              iconTheme: MaterialStateProperty.resolveWith((state) {
+                if (state.contains(MaterialState.selected)) {
+                  return const IconThemeData(color: AppColors.primary);
+                }
+                return IconThemeData(color: Colors.black.withOpacity(0.7));
+              }),
+              labelTextStyle: MaterialStateProperty.resolveWith((state) {
+                if (state.contains(MaterialState.selected)) {
+                  return const TextStyle(
+                      color: AppColors.primary, fontSize: 12, height: 0.7);
+                }
+                return TextStyle(
+                    color: Colors.black.withOpacity(0.7),
+                    fontSize: 12,
+                    height: 0.7);
+              }),
+            ),
+            useMaterial3: true,
           ),
-          useMaterial3: true,
+          home: const BottomNavigation(),
         ),
-        home: const BottomNavigation(),
       ),
     );
   }
