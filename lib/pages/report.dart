0af5035e7ffc3647a6d1220d1cbbeb009e0e2a76
Kevin v Loon <74180193+KvnvL@users.noreply.github.com>
2023-11-23 15:13:45 +0100

    add: other input types

--- lib/pages/report.dart
+++ lib/pages/report.dart
@@ -1,7 +1,10 @@
 import 'dart:ffi';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:wildlife_nl_app/utilities/app_colors.dart';
 import 'package:wildlife_nl_app/utilities/app_icons.dart';
 import 'package:wildlife_nl_app/utilities/app_styles.dart';
@@ -24,14 +27,104 @@ class AnimalType {
 }
 
 class AnimalQuestion {
+  final String inputType;
   final String question;
+  final bool required;
   final bool fullWidth;
   final String hint;
   final List<String> options;
-  final String answer;
 
-  AnimalQuestion({required this.question, required this.fullWidth, required this.hint, required this.options, required this.answer});
+  AnimalQuestion({required this.inputType, required this.question,required this.required, required this.fullWidth, required this.hint, required this.options});
 }
+
+final List<AnimalQuestion> animalQuestions = [
+  AnimalQuestion(
+    inputType: 'dropdown',
+    question: "Aantal dieren:",
+    required: true,
+    fullWidth: false,
+    hint: "Selecteer aantal",
+    options: ['0','1','2','3','4','5+'],
+  ),
+  AnimalQuestion(
+    inputType: 'dropdown',
+    question: "Aantal jonge:",
+    required: true,
+    fullWidth: false,
+    hint: "Selecteer aantal",
+    options: ['0','1','2','3','4','5+'],
+  ),
+  AnimalQuestion(
+    inputType: 'dropdown',
+    question: "Wat was je aan het doen toen je het dier zag?",
+    required: false,
+    fullWidth: true,
+    hint: "Selecteer handeling",
+    options: [
+      'Er op af rennen',
+      'Luide geluiden maken',
+      'Langzaam er naar toe lopen',
+      'Een selfie maken',
+      'Wegrennen',
+      'Een foto maken',
+      'Langs het dier lopen',
+      'Stil blijven staan',
+      'Naar het dier kijken',
+      'Langzaam weglopen',
+    ],
+  ),
+  AnimalQuestion(
+      inputType: 'horizontalOptions',
+      question: 'Hoe voelde je je toen je het dier zag?',
+      required: false,
+      fullWidth: true,
+      hint: '',
+      options: ['angstig', 'neutral', 'enthousiast']
+  ),
+  AnimalQuestion(
+    inputType: 'dropdown',
+    question: "Wat deed het dier?",
+    required: false,
+    fullWidth: true,
+    hint: "Selecteer handeling",
+    options: [
+      'Het deed zijn eigen ding',
+      'Het toonde interesse in mij',
+      'Het rende weg van mij',
+      'Het toonde agressief gedrag',
+    ],
+  ),
+  AnimalQuestion(
+      inputType: 'horizontalOptions',
+      question: 'Hoe denk je dat het dier zich voelde?',
+      required: false,
+      fullWidth: true,
+      hint: '',
+      options: ['angstig', 'neutraal', 'enthousiast']
+  ),
+  AnimalQuestion(
+    inputType: 'stars',
+    question: 'Hoe zou je de interactie beoordelen?',
+    required: false,
+    fullWidth: true,
+    hint: '',
+    options: [],
+  ),
+  AnimalQuestion(
+    inputType: 'photo',
+    question: "Heb je foto's van de waarneming?",
+    required: false,
+    fullWidth: true,
+    hint: '',
+    options: [],
+  ),
+];
+
+final animalQuestionsMap = animalQuestions.asMap();
+
+List<String> _evaluationAnswers = List.filled(animalQuestions.length, "");
+String _chosenType = '';
+String _chosenName = '';
 
 class ReportPage extends StatefulWidget {
   ReportPage({super.key});
@@ -42,8 +135,6 @@ class ReportPage extends StatefulWidget {
 
 class _ReportPageState extends State<ReportPage> {
   int _currentStep = 0;
-  String _chosenType = '';
-  String _chosenName = '';
 
   final List<AnimalType> diersoorten = [
     AnimalType(
@@ -80,41 +171,6 @@ class _ReportPageState extends State<ReportPage> {
       animals: [
         Animals(name: "Wisent", img: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"),
       ],
-    ),
-  ];
-
-  final List<AnimalQuestion> animalQuestions = [
-    AnimalQuestion(
-      question: "Aantal dieren:",
-      fullWidth: false,
-      hint: "Selecteer aantal",
-      options: ['0','1','2','3','4','5+'],
-      answer: "",
-    ),
-    AnimalQuestion(
-      question: "Aantal jonge:",
-      fullWidth: false,
-      hint: "Selecteer aantal",
-      options: ['0','1','2','3','4','5+'],
-      answer: "",
-    ),
-    AnimalQuestion(
-      question: "Wat was je aan het doen toen je het dier zag?",
-      fullWidth: true,
-      hint: "Selecteer handeling",
-      options: [
-        'Er op af rennen',
-        'Luide geluiden maken',
-        'Langzaam er naar toe lopen',
-        'Een selfie maken',
-        'Wegrennen',
-       'Een foto maken',
-        'Langs het dier lopen',
-        'Stil blijven staan',
-        'Naar het dier kijken',
-        'Langzaam weglopen',
-      ],
-      answer: "",
     ),
   ];
 
@@ -186,9 +242,24 @@ class _ReportPageState extends State<ReportPage> {
     )).toList();
   }
 
+  Future<void> _pickImage(ImageSource source, i) async {
+    try {
+      final pickedFile = await ImagePicker().pickImage(source: source);
+
+      if (pickedFile != null) {
+        // Do something with the picked image file
+        // print("Image picked: ${pickedFile.path}");
+        _evaluationAnswers[i] = pickedFile.path;
+      } else {
+        print("No image selected");
+      }
+    } catch (e) {
+      print("Error picking image: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(toolbarHeight: 0, excludeHeaderSemantics: true,),
       body: Column(
@@ -351,12 +422,141 @@ class _ReportPageState extends State<ReportPage> {
                         child: Wrap(
                           spacing: 16,
                           runSpacing: 16,
-                          children: animalQuestions.map((question) => MyDropdown(
-                              options: question.options,
-                              title: question.question,
-                              placeholder: question.hint,
-                              fullWidth: question.fullWidth
-                          )).toList(),
+                          children: animalQuestionsMap.map((i, question) {
+                            if (question.inputType == "dropdown") {
+                              return MapEntry(i,
+                                  MyDropdown(
+                                    options: question.options,
+                                    title: question.question,
+                                    placeholder: question.hint,
+                                    fullWidth: question.fullWidth,
+                                    required: question.required,
+                                    index: i,
+                                  )
+                              );
+                            } else if (question.inputType == 'horizontalOptions') {
+                              return MapEntry(i, Container(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      question.question,
+                                      style: AppStyles.of(context).data.textStyle.cardTitle.copyWith(
+                                        color: AppColors.primary,
+                                      ),
+                                    ),
+                                    const SizedBox(height:8),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: question.options.map((option) => GestureDetector(
+                                        onTap: (){
+                                          if(_evaluationAnswers[i] != option) {
+                                            setState(() {
+                                              _evaluationAnswers[i] = option;
+                                            });
+                                          } else {
+                                              setState(() {
+                                                _evaluationAnswers[i] = '';
+                                              });
+                                          }
+                                        },
+                                        child: Container(
+                                            decoration: ShapeDecoration(
+                                              color: Colors.white,
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(8),
+                                                side: BorderSide(
+                                                  color: _evaluationAnswers[i] == option ? AppColors.primary : Colors.white,
+                                                  width: 2,
+                                                ),
+                                              ),
+                                            ),
+                                            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                                            child: Text(option)
+                                        ),
+                                      )).toList(),
+                                    )
+                                  ],
+                                ),
+                              ));
+                            }
+                            else if (question.inputType == 'stars'){
+                              return MapEntry(i, Container(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      question.question,
+                                      style: AppStyles.of(context).data.textStyle.cardTitle.copyWith(
+                                        color: AppColors.primary,
+                                      ),
+                                    ),
+                                    RatingBar.builder(
+                                      minRating: 1,
+                                      direction: Axis.horizontal,
+                                      allowHalfRating: true,
+                                      itemCount: 5,
+                                      itemPadding: EdgeInsets.symmetric(horizontal: 0),
+                                      itemBuilder: (context, _) => Icon(
+                                        Icons.star,
+                                        color: AppColors.primary,
+                                      ),
+                                      onRatingUpdate: (rating) {
+                                        if(_evaluationAnswers[i] != rating) {
+                                            _evaluationAnswers[i] = rating.toString();
+                                        }
+                                      },
+                                    ),
+                                  ]
+                              )
+                              ));
+                            }
+                            else if (question.inputType == 'photo') {
+                              return MapEntry(i, Container(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      question.question,
+                                      style: AppStyles.of(context).data.textStyle.cardTitle.copyWith(
+                                        color: AppColors.primary,
+                                      ),
+                                    ),
+                                    const SizedBox(height:8),
+                                    Row(
+                                      children: [
+                                        ElevatedButton(
+                                          onPressed: () {
+                                            _pickImage(ImageSource.gallery, i); // Open gallery
+                                          },
+                                          child: Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              Icon(AppIcons.add, size: 20),
+                                              SizedBox(width: 4),
+                                              Text("Voeg foto toe"),
+                                            ],
+                                          ),
+                                          style: ElevatedButton.styleFrom( // Achtergrondkleur van de knop
+                                            onPrimary: AppColors.primary, // Tekstkleur van de knop
+                                            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8), // Padding van de knop
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(8), // Afgeronde hoeken
+                                                side: BorderSide(color: AppColors.primary, width: 2)
+                                            ),
+                                            elevation: 0,
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ]
+                                )
+                              ));
+                            }
+                            else {
+                              return MapEntry(i, Container());
+                            }
+                          }).values.toList(),
                         )
                       )
                   ),
@@ -397,7 +597,7 @@ class _ReportPageState extends State<ReportPage> {
                     ),
                     Visibility(
                       visible: _currentStep > 0,
-                      child: SizedBox(width: 16.0), // Ruimte toevoegen tussen de knoppen als de eerste knop zichtbaar is
+                      child: SizedBox(width: 16.0), // Space between buttons if 2nd button is visible
                     ),
                     Expanded(
                       child: ElevatedButton(
@@ -409,7 +609,15 @@ class _ReportPageState extends State<ReportPage> {
                           setState(() {
                             _currentStep++;
                           });
-                        } : null),
+                          } : ((_evaluationAnswers[0].isNotEmpty && _evaluationAnswers[1].isNotEmpty) ? () {
+                          print(_evaluationAnswers);
+                          Navigator.of(context).pop();
+                          _currentStep = 0;
+                          _chosenName = '';
+                          _chosenType= '';
+                          _evaluationAnswers = List.filled(animalQuestions.length, "");
+                        }  : null)),
+                        // onPressed: null,
                         child: Text(
                           _currentStep < 2 ? 'Volgende' : 'Opslaan',
                           style: AppStyles.of(context).data.textStyle.buttonText
@@ -440,9 +648,11 @@ class MyDropdown extends StatefulWidget {
   final List<String> options;
   final String title;
   final String placeholder;
+  final bool required;
   final bool fullWidth;
+  final int index;
 
-  MyDropdown({required this.options, required this.title, required this.placeholder, required this.fullWidth});
+  MyDropdown({required this.options, required this.title, required this.placeholder, required this.required, required this.fullWidth, required this.index});
 
   @override
   _MyDropdownState createState() => _MyDropdownState();
@@ -457,7 +667,7 @@ class _MyDropdownState extends State<MyDropdown> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          widget.title,
+          (widget.title + (widget.required ? '*' : '')),
           style: AppStyles
               .of(context)
               .data
@@ -490,7 +700,11 @@ class _MyDropdownState extends State<MyDropdown> {
                 filled: true,
                 contentPadding: EdgeInsets.symmetric(horizontal: 8)
               ),
-
+              onSelected: (String? value) {
+                  setState(() {
+                    _evaluationAnswers[widget.index] = value!;
+                  });
+              },
               dropdownMenuEntries: widget.options.map<DropdownMenuEntry<String>>((String value) {
                 return DropdownMenuEntry<String>(
                   value: value,
