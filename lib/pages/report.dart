6d340062363e81d0fd320124279cee07a035212d
Kevin v Loon <74180193+KvnvL@users.noreply.github.com>
2024-01-11 14:43:25 +0100

    fix: showing species with 'gedrag' report type

--- lib/pages/report.dart
+++ lib/pages/report.dart
@@ -301,7 +301,7 @@ class _ReportPageState extends ConsumerState<ReportPage> {
                             : (_currentStep > 0
                                 ? CustomStepState.complete
                                 : CustomStepState.editing),
-                        content: pickAnimalStep(animals),
+                        content: pickAnimalStep(animals, 'Welk dier heb je gezien?'),
                       ),
                       CustomStep(
                           state: _currentStep >= 1
@@ -312,9 +312,25 @@ class _ReportPageState extends ConsumerState<ReportPage> {
               InteractionTypeKey.damage => Container(
                   padding: EdgeInsets.symmetric(horizontal: 16),
                   child: evaluationQuestions(questions)),
-              InteractionTypeKey.inappropriateBehaviour => Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: evaluationQuestions(questions)),
+              InteractionTypeKey.inappropriateBehaviour => CustomStepper(
+                  elevation: 0,
+                  currentStep: _currentStep,
+                  controlsBuilder: (context, details) => const Text(""),
+                  steps: [
+                    CustomStep(
+                      state: _currentStep < 0
+                          ? CustomStepState.indexed
+                          : (_currentStep > 0
+                          ? CustomStepState.complete
+                          : CustomStepState.editing),
+                      content: pickAnimalStep(animals, 'Bij welk dier is er iets gebeurd?'),
+                    ),
+                    CustomStep(
+                        state: _currentStep >= 1
+                            ? CustomStepState.editing
+                            : CustomStepState.indexed,
+                        content: evaluationQuestions(questions)),
+                  ]),
               InteractionTypeKey.traffic => CustomStepper(
                     elevation: 0,
                     currentStep: _currentStep,
@@ -326,7 +342,7 @@ class _ReportPageState extends ConsumerState<ReportPage> {
                             : (_currentStep > 0
                                 ? CustomStepState.complete
                                 : CustomStepState.editing),
-                        content: pickAnimalStep(animals),
+                        content: pickAnimalStep(animals, 'Welk dier heb je gezien?'),
                       ),
                       CustomStep(
                           state: _currentStep >= 1
@@ -394,9 +410,18 @@ class _ReportPageState extends ConsumerState<ReportPage> {
                       InteractionTypeKey.damage => () {
                           _closeReport(false);
                         },
-                      InteractionTypeKey.inappropriateBehaviour => () {
-                          _closeReport(false);
-                        },
+                      InteractionTypeKey.inappropriateBehaviour =>
+                      (_chosenAnimal.isNotEmpty && _currentStep == 0)
+                          ? () {
+                        setState(() {
+                          _currentStep++;
+                        });
+                      }
+                          : ((_currentStep == 1)
+                          ? () {
+                        _closeReport(false);
+                      }
+                          : null),
                       InteractionTypeKey.traffic =>
                         (_chosenAnimal.isNotEmpty && _currentStep == 0)
                             ? () {
@@ -430,7 +455,7 @@ class _ReportPageState extends ConsumerState<ReportPage> {
                             _currentStep < 1 ? 'Volgende' : 'Opslaan',
                           InteractionTypeKey.damage => "Opslaan",
                           InteractionTypeKey.inappropriateBehaviour =>
-                            "Opslaan",
+                            _currentStep < 1 ? 'Volgende' : 'Opslaan',
                           InteractionTypeKey.traffic =>
                             _currentStep < 1 ? 'Volgende' : 'Opslaan',
                           InteractionTypeKey.maintenance => "Opslaan",
@@ -446,7 +471,7 @@ class _ReportPageState extends ConsumerState<ReportPage> {
     );
   }
 
-  Widget pickAnimalStep(animals) {
+  Widget pickAnimalStep(animals, animalQuestion) {
     return SizedBox(
       width: double.maxFinite,
       child: Column(
@@ -455,7 +480,7 @@ class _ReportPageState extends ConsumerState<ReportPage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            'Welk dier heb je gezien?',
+            animalQuestion,
             style: AppStyles.of(context).data.textStyle.cardTitle.copyWith(
                   color: AppColors.primary,
                 ),
